fb98b495cee1c858faa5a58553e536399755af23
QMDMQ <twobsneaky@gmail.com>
2025-03-25 10:04:54 +0700

    3 Features Complete in 80%, Database not Implement

--- StudentInformationManagementSystem/Controllers/HomeController.cs
+++ StudentInformationManagementSystem/Controllers/HomeController.cs
@@ -1,31 +1,41 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using StudentInformationManagementSystem.Models;
+using System.Diagnostics;
 
-namespace StudentInformationManagementSystem.Controllers;
-
-public class HomeController : Controller
+namespace StudentInformationManagementSystem.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            // If the user is not logged in, redirect to login page
+            // Uncomment this to force login before accessing home page
+            /*
+            if (HttpContext.Session.GetInt32("UserId") == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+            */
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
